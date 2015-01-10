0c61c18cf57c7e6d28a4cdc06e34a49b21d0459a
Simon Funke <simon.funke@gmail.com>
2015-01-10 15:09:40 +0100

    Update version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 setup(name='PyRPLidar',
-      version='0.2',
-      author='Tom Wang (edited by Simon Funke)',
+      version='0.2.1',
+      author='Tom Wang and Simon Funke',
       author_email='simon.funke@gmail.com',
       url='https://github.com/funsim/PyRPLidar',
       packages = ['rplidar']
